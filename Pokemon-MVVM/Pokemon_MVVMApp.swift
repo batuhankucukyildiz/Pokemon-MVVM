28910e32884a0c1ed8cc579dc6bf39aaed611e9f
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-08-18 08:33:36 +0300

    Initial Commit

---
+++ Pokemon-MVVM/Pokemon_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_MVVMApp.swift
+//  Pokemon-MVVM
+//
+//  Created by Batuhan Kucukyildiz on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
